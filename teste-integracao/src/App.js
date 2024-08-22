48b874531ac509c5aa12a103f1faf7127397dfbf
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-08-22 10:23:41 -0300

    'primeiro commit'

---
+++ teste-integracao/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        bem vindo ao projeto exemplo da materia de integração e entrega continua!!!!
+      </h1>
+      <div>
+        <h2>Olá pipelines!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;

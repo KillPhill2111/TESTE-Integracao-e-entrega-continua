66c72bb502cdbf89fa211d0dca4edaac7a3b5163
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-08-29 12:48:03 -0300

    'sgundo commit- modificação do projeto e criação do arquivo ci.yml'

--- teste-integracao/src/components/ListaCompras/ListaCompras.jsx
+++ teste-integracao/src/components/ListaCompras/ListaCompras.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React ,{ useState }  from 'react'
 import AddItens from '../AddItens/AddItens'
 
 function ListaCompras(itens, lista) {
+  const [items, setItems]=useState([])
+  const [inputValue, setInputValue]= useState('')
+
+
+  const addItem=()=>{
+    if(inputValue.trim()!== ''){
+      setItems([...items, inputValue])
+      setInputValue('');
+    } 
+  };
+  const removeItem = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
   return (
-    <ul class='lista'>
-        <h2>Listinha de compras</h2>
-        
-    </ul>
+    <div>
+      <h1>Lista de Compras</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Adicione um item"
+      />
+      <button onClick={addItem}>Adicionar</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
